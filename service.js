703f56c427e6e6a56db7ed6497d082a035663498
Suz Hinton <suz.hinton@gmail.com>
2014-01-04 16:08:21 -0800

    testing something quick

--- service.js
+++ service.js
@@ -29,29 +29,14 @@ http.createServer(function(req, res) {
       if (req.method === 'POST') {
         
         var data = qs.parse(body);
-        console.log(data);
-        
-        // change this to the att api
-        request.post({
-          url : 'https://api.mailgun.net/v2/suziam.com/messages',
-          auth: {
-            user: 'api',
-            password: 'key-2gsxuklh9jx3-4gqjrycm0swqjofua67',
-          },
-          form: data
-          
-          // change this reponse to the atti or level db data en
-        }, function(err, mailgunResponse) {
-          console.log(mailgunResponse.body, mailgunResponse.statusCode);
-          if (err) {
-            res.writeHead(400);
-            res.end((err && err.message) ? err.message : 'unknown error');
-            console.log(console.log('Oh noes: ' + err))
-          } else {
-            console.log('mail sent on behalf of', data.from);
-            res.end('ok');
-          }
+        //console.log(data);
+
+        watson.sendSMS('cat3.jpg', 'e3ICwFwy0CBXy2IUTDyYJzuj0Tq0rzzj', function(err, t) {
+          console.log('response : ', err, t);
         });
+        
+       
+      
       }
     });    
   }
